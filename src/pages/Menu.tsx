f8c631d8754fd22ad6259fa28c3d768a89574649
sahra104 <sahra.abdikadir@strathmore.edu>
2026-06-23 19:42:14 +0300

    Refactor: Group menu items into distinct categories

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -2,23 +2,23 @@ function Menu() {
     return (
         <div className="page menu">
             <div className="overlay">
-                <h1>Menu</h1>
+                <h1>Our Menu</h1>
 
+                <h2>Traditional Pastries</h2>
                 <div className="card">
                     <h3>Kac Kac</h3>
                     <p>Ksh 250</p>
                 </div>
-
                 <div className="card">
                     <h3>Malawax</h3>
                     <p>Ksh 300</p>
                 </div>
-
                 <div className="card">
                     <h3>Sambuus</h3>
                     <p>Ksh 200</p>
                 </div>
 
+                <h2>Hot Drinks</h2>
                 <div className="card">
                     <h3>Shaah Cadays</h3>
                     <p>Ksh 150</p>
